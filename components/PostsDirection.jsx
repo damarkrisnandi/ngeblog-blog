95b08b65a18e009edff1a5bf71d8ff0fa718a799
damar <damar@gitlab.lab>
2022-07-22 23:00:44 +0700

    fix href

--- components/PostsDirection.jsx
+++ components/PostsDirection.jsx
@@ -14,7 +14,7 @@ const NextDirection = ({ data, posts }) => {
     const index = posts.findIndex(d => d.data.title === data.title);
     const nextPost = posts[posts.findIndex(d => d.data.title === data.title) - 1];
     return index > 0 && index <= posts.length - 1 ? (
-        <Link href="/[slug]" as={`/post/${nextPost.slug}`}>
+        <Link href="/post/[slug]" as={`/post/${nextPost.slug}`}>
             <button className='flex flex-col justify-center item-start p-5 border-2 rounded-lg 
                 transition-all duration-200 delay-75
                 hover:bg-blue-600 hover:text-white
